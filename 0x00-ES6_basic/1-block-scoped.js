7ab7a5e8e4d76a409879d48143eea4a7ebae0622
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-10-01 03:12:52 +0100

    block scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,9 +2,5 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  // eslint-disable-line no-empty
-  if (trueOrFalse) {
-  }
-
   return [task, task2];
 }
