0fd11f0e3e7d0de16eb562555fd0cb0cef5d81a5
arabson99 <111277887+arabson99@users.noreply.github.com>
2024-10-09 19:45:28 +0100

    Updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,3 @@
-/**
- * Joins a set of strings with a dash after stripping the strings of
- * a leading sub string.
- * @param {Set<String>} set - A collection of strings.
- * @param {String} startString - The string to strip from the beginning
- * of each item in the set.
- * @returns {String}
- */
 export default function cleanSet (set, startString) {
   const parts = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
