4557f9bf63755ff41e862417249921cc01ccdec4
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-12-02 17:11:48 +0100

    NodeJS Basics Task all done

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -7,24 +7,25 @@ function readDatabase(path) {
     fs.readFile(path, 'utf-8', (error, data) => {
       if (error) {
         reject(new Error('Cannot load the database'));
-      }
-      const lines = data.split('\n');
-      const dataLines = lines.slice(1);
+      } else {
+        const lines = data.split('\n');
+        const dataLines = lines.slice(1);
 
-      dataLines.forEach((line) => {
-        if (line.trim() !== '') {
-          const [firstName, , , field] = line.split(',');
+        dataLines.forEach((line) => {
+          if (line.trim() !== '') {
+            const [firstName, , , field] = line.split(',');
 
-          if (firstName && field) {
-            if (!students[field]) {
-              students[field] = [];
+            if (firstName && field) {
+              if (!students[field]) {
+                students[field] = [];
+              }
+              students[field].push(firstName);
             }
-            students[field].push(firstName);
           }
-        }
-      });
+        });
 
-      resolve(students);
+        resolve(students);
+      }
     });
   });
 }
